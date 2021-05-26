712cfd3803c7ad91fd14b24aa4b614262d20cb28
Pascal Betti <pascal.betti07@gmail.com>
2021-05-27 00:05:15 +0200

    Fixed window size

--- Levels/Level_End.gd
+++ Levels/Level_End.gd
@@ -4,9 +4,6 @@ var P1_Entered = false
 var P2_Entered = false
 
 var scene = Dialogic.start("level1")
-		
-func after_dialog(_timeline_name):
-	get_tree().change_scene("res://Levels/Level2.tscn")
 
 func _on_Level_End_body_entered(body):
 	if body == get_node("../Player"):
@@ -16,9 +13,14 @@ func _on_Level_End_body_entered(body):
 	if (P1_Entered && P2_Entered):
 		scene.connect("timeline_end", self, "after_dialog")
 		get_parent().add_child(scene)
+		get_node("../Player").queue_free()
+		get_node("../Player2").queue_free()
 
 func _on_Level_End_body_exited(body):
 	if body == get_node("../Player"):
 		P1_Entered = false
 	if body == get_node("../Player2"):
 		P2_Entered = false
+
+func after_dialog(_timeline_name):
+	get_tree().change_scene("res://Levels/Level2.tscn")
